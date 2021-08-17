5152ec275a57aa62837f031288543ef2fa372fbc
belmiro-kunga <belmirokunga@gmail.com>
2021-08-17 13:29:52 +0100

    introdução e declaração de variáveis
    introdução e declaração de variáveis

---
+++ Saida/script.js
@@ -0,0 +1,5 @@
+//document.getElementById("kunga").innerHTML = 5 + 7;
+
+//alert(3 * 5)
+
+//console.log(3-45)
---
+++ Variaveis/script.js
@@ -0,0 +1,26 @@
+//JavaScript Variables
+
+//var x =  5;
+
+//var y = 6;
+
+//var z = x + y;
+
+//console.log(z)
+
+
+//var preco1 = 12;
+//var preco2 = 34;
+
+//var total = preco1 + preco2;
+
+
+//console.log(total)
+
+
+var _nomeCompleto = "belmiro Kunga";
+
+console.log(_nomeCompleto);
+
+
+
